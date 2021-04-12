4cc6bfb6d9d161ec7abeab365ef184a46fc7c1a7
5d-jh <jeonghyun0126@outlook.com>
2021-04-13 03:21:45 +0900

    test: Modify test case more strictly

--- test/github-issues/7523/issue-7523.ts
+++ test/github-issues/7523/issue-7523.ts
@@ -2,6 +2,7 @@ import "reflect-metadata";
 import {Connection} from "../../../src";
 import {createTestingConnections, closeTestingConnections} from "../../utils/test-utils";
 import {ChildEntity1, ChildEntity2} from "./entity/Test";
+import {expect} from "chai";
 
 describe("github issues > #7523 Do not create duplicate CREATE TYPE migration query when same 'enumName's are exists", () => {
     let connections: Connection[];
@@ -13,16 +14,16 @@ describe("github issues > #7523 Do not create duplicate CREATE TYPE migration qu
     }));
     after(() => closeTestingConnections(connections));
 
-    it("should recognize model changes", () => Promise.all(connections.map(async connection => {
+    it("should not generate duplicate CREATE TYPE queries", () => Promise.all(connections.map(async connection => {
         const sqlInMemory = await connection.driver.createSchemaBuilder().log();
-        sqlInMemory.upQueries.length.should.be.greaterThan(0);
-        sqlInMemory.downQueries.length.should.be.greaterThan(0);
+        expect(sqlInMemory.upQueries.length).eq(3);
+        expect(sqlInMemory.downQueries.length).eq(3);
     })));
 
     it("should not generate queries when no model changes", () => Promise.all(connections.map(async connection => {
         await connection.driver.createSchemaBuilder().build();
         const sqlInMemory = await connection.driver.createSchemaBuilder().log();
-        sqlInMemory.upQueries.length.should.be.equal(0);
-        sqlInMemory.downQueries.length.should.be.equal(0);
+        expect(sqlInMemory.upQueries.length).eq(0);
+        expect(sqlInMemory.downQueries.length).eq(0);
     })));
 });
